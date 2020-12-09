523f9f31c4a1105627067328c5b19b8888822a47
Gad Jacobs <gadexofficial@gmail.com>
2020-12-09 03:40:27 +0100

    career page

--- src/components/dashboard/career/career.js
+++ src/components/dashboard/career/career.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 import Splash from "../../splashscreen/splash";
 import CareerSummary from "./careerSummary";
 
-export default function Career({ title }) {useEffect(() => {
+export default function Career({ title }) {
+  useEffect(() => {
     setTimeout(() => {
       setTimePassed();
     }, 1500);
   });
+
+  const occupations = ['Street Artist', 'Illustrator', 'Artistic Painter', 'Landscape Architect', 'Sculptor', 'Glass Artist', 'Cartoonist', 'Dancer', 'Animator', '3D Modeller', 'Architect', 'Musician']
 
   const [loading, setLoading] = useState(true);
 
@@ -30,6 +33,67 @@ export default function Career({ title }) {useEffect(() => {
           </h2>
           <div className="flex flex-wrap -m-4 text-center">
             <CareerSummary />
+          </div>
+        </div>
+      </div>
+      <div className="mt-4 pt-6 bg-white">
+        <div id="chartOne" width="400" height="200">
+          <h2 className="underline text-2xl text-center text-black pb-6">
+            There are six personality types in Holland's model:
+          </h2>
+          <p className="text-xl font-bold text-center text-black pb-6">
+            Realistic:{" "}
+            <span className="text-lg font-thin text-center text-black pb-6">
+              practical, physical, concrete, hands-on, machine, and
+              tool-oriented
+            </span>
+          </p>
+          <p className="text-xl font-bold text-center text-black pb-6">
+            Investigative:{" "}
+            <span className="text-lg font-thin text-center text-black pb-6">
+              analytical, intellectual, scientific, explorative, thinker
+            </span>
+          </p>
+          <p className="text-xl font-bold text-center text-black pb-6">
+            Artistic:{" "}
+            <span className="text-lg font-thin text-center text-black pb-6">
+              creative, original, independent, chaotic, inventive, media,
+              graphics, and text
+            </span>
+          </p>
+          <p className="text-xl font-bold text-center text-black pb-6">
+            Social:{" "}
+            <span className="text-lg font-thin text-center text-black pb-6">
+              cooperative, supporting, helping, healing/nurturing, teaching
+            </span>
+          </p>
+          <p className="text-xl font-bold text-center text-black pb-6">
+            Enterprising:{" "}
+            <span className="text-lg font-thin text-center text-black pb-6">
+              competitive environments, leadership, persuading, status
+            </span>
+          </p>
+          <p className="text-xl font-bold text-center text-black pb-6">
+            Conventional:{" "}
+            <span className="text-lg font-thin text-center text-black pb-6">
+              detail-oriented, organizing, clerical
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="mt-6 p-6 bg-white">
+        <div id="chartOne" width="400" height="200">
+          <h2 className="text-2xl text-center text-black pb-6">
+            Gad's list of suggested occupations
+          </h2>
+          <div class="bg-white overflow-auto whitespace-no-wrap py-3 px-4 text-center">
+            {occupations.map((occupation, i) => {
+              return (
+                <button class="inline-block border border-teal-500 text-teal-500 rounded-full px-6 py-2 mr-4">
+                  {occupation}
+                </button>
+              );
+            })}
           </div>
         </div>
       </div>
